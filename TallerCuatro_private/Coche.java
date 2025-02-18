28a839eb25e1596593aa6d02be81049c36f7e1bd
JaiderRodriguez-B <jaider.rodriguezbuelvas@unicolombo.edu.co>
2025-02-17 20:25:14 -0500

    taller cuatro sgundo avance

---
+++ TallerCuatro_private/Coche.java
@@ -0,0 +1,25 @@
+package TallerCuatro_private;
+
+public class Coche {
+
+    private String marca, modelo;
+    private double velocidad;
+
+    public String getModelo(){
+        return modelo;
+    }
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void acelerar(double incremento) {
+        if (incremento > 0) {
+            velocidad += incremento;
+        }
+    }
+}
--- TallerCuatro_private/clasePrueba.java
+++ TallerCuatro_private/clasePrueba.java
@@ -7,5 +7,10 @@ public class clasePrueba {
         System.out.println("\nNombre: "+estudiante1.getNombre());
         System.out.println("Edad: "+estudiante1.getEdad());
         System.out.println("Nota Promedio: "+ estudiante1.getNotaPromedio());
+
+        Coche coche1 = new Coche();
+        coche1.getMarca();
+        
+        //System.out.println("\n" + coche1.acelerar(10.5));
     }
 }
